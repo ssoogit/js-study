6e4b028d37898dc3a8eeb259b62666118e2abac8
ssoogit <150372021+ssoogit@users.noreply.github.com>
2026-05-19 21:41:57 +0900

    feat: 9강 조건문 예제 코드 추가

---
+++ lecture/9강_조건문.js
@@ -0,0 +1,22 @@
+// ──────────────────────────────
+// switch case 조건문
+// ──────────────────────────────
+
+let fruit = 'apple';
+
+switch (fruit) {
+    case 'banana':
+        console.log('바나나');
+        break;
+    case 'orange':
+        console.log('오렌지');
+        break;
+    case 'apple':
+        console.log('사과');
+        break;
+    case 'grape':
+        console.log('포도');
+        break;
+    default:
+        console.log('다른 종류의 과일');
+}
